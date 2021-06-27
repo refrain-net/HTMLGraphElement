cbdae39e4ca16585d07cdba7a78bdf2a5e07b70a
Refrain <72150044+refrain-net@users.noreply.github.com>
2021-06-27 15:54:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 'use strict';
 
-Array.prototype.transpose = function () {
-  const array = [...this];
-  array.forEach((column, columnIndex) => column.forEach((row, rowIndex) => (this[rowIndex] = this[rowIndex] || []).push(row)));
-  return this;
-};
-
 const graph = document.querySelector('#graph');
 const render = document.querySelector('#render');
 const elements = [];
@@ -39,12 +33,12 @@ function load (file) {
   reader.addEventListener('load', event => {
     const {result} = reader;
 
-    const arr = result.replace(/\r/g, '').split('\n').filter(line => line !== '').map(line => line.split(','));
+    let arr = result.replace(/\r/g, '').split('\n').filter(line => line !== '').map(line => line.split(','));
 
     const createFormItem = name => `<input name = '${name}' type = 'checkbox' /><label for = '${name}'>${name}</label>`;
     checks.innerHTML = arr[0].reduce((html, name) => html + createFormItem(name), '');
 
-    arr.transpose();
+    arr = arr.transpose();
 
     graph.setOrigin(HTMLGraphElement.ORIGIN_LEFT | HTMLGraphElement.ORIGIN_BOTTOM);
     graph.setRangeX(0, arr.length);
